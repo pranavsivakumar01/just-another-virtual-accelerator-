8be61b19dce87095183b2cbba4fa8711451e4acd
pranavsivakumar01 <pranavas.22aim@kongu.edu>
2025-07-27 11:19:11 +0530

    constructor code

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+
+class Constructor {
+
+    // default constructor
+    public Constructor() {
+        System.out.print("This is my constructor");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Constructor obj = new Constructor(); // Constructor gets called
+    }
+}
